2bca9d7510b64422a7cd22f9203983b84e6baaea
Kirwa01 <tunenmordecai98@gmail.com>
2023-10-05 14:06:23 -0700

    Printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/*
+ * main - opens the main function
+ *
+ * Return: ends the code
+ */
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art");
+
+	return (0);
+
+
+}
