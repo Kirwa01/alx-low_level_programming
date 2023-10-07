13bc0196f0349b4017f51a812e1f4d23153be528
Kirwa01 <tunenmordecai98@gmail.com>
2023-10-07 02:36:58 -0700

    Task 2

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - start of line
+ *
+ * Return: denoted 0
+ */
+
+int main(void)
+{
+char i;
+
+for (i = 'a'; i <= 'z' ; i++)
+	putchar(i);
+putchar('\n');
+
+return (0);
+}
