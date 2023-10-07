532c55ade0e373633ef7001a4d11381cfd6ff2df
Kirwa01 <tunenmordecai98@gmail.com>
2023-10-07 05:15:14 -0700

    Task 7

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - start point
+ *
+ * Return: success
+ */
+
+int main(void)
+{
+	char i;
+
+	for (i = 'z' ; i >= 'a' ; i--)
+		putchar(i);
+	putchar('\n');
+	return (0);
+
+
+
+
+}
