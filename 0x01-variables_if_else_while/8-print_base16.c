4856aaa895e8a7afd9a0e44506ae687226348f85
Kirwa01 <tunenmordecai98@gmail.com>
2023-10-07 05:30:19 -0700

    Base 16

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+/**
+ * main - start point
+ *
+ * Return: success
+ */
+int main(void)
+{
+	int i;
+	char y;
+
+	for (i = 0 ; i < 10 ; i++)
+		putchar(i + '0');
+	for (y = 'a' ; y <= 'f' ; y++)
+		putchar(y);
+	putchar('\n');
+	return (0);
+
+
+
+
+
+
+
+
+
+
+
+}
