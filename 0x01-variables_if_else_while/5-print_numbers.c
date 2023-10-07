3b2fd153c2f635ece02ee6460ab3294c6b2e6b55
Kirwa01 <tunenmordecai98@gmail.com>
2023-10-07 04:43:18 -0700

    Numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+/**
+ * main - start point
+ *
+ * Return: denoted 0
+ */
+
+int main(void)
+{
+	int i;
+
+	for (i = 0 ; i <= 10 ; i++)
+		printf("%d", i);
+	printf("\n");
+	return (0);
+
+
+
+
+
+
+
+
+}
