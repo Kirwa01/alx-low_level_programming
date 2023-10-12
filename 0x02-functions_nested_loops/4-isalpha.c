d6b454a11b561334c19416e8ead2b24fda8f07d7
Kirwa01 <tunenmordecai98@gmail.com>
2023-10-12 01:35:01 -0700

    function that checks for alphabet character

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -7,10 +7,10 @@
  *Return: 1 if it is true and 0 if false
  */
 int _isalpha(int c);
+
 {
-	if ((c => 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
+	if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
 		return (1);
 	else
 		return (0);
-		
 }
