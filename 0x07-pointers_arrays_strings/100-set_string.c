abe4c18aaf8916403028f7cd1182759068638773
Kirwa01 <tunenmordecai98@gmail.com>
2023-10-25 03:34:26 -0700

    sum of two diagonals of a square matrix of integers

---
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * set_string - sets the value of a pointer to a char
+ * @s: value of the pointer
+ * @to: character the value is being set to
+ * Return: returns the value of a pointer in a character
+ */ 
+void set_string(char **s, char *to)
+{
+
+}
