fc57ece0484e36ffe5f954b93b066dc2e49d1248
Kirwa01 <tunenmordecai98@gmail.com>
2023-11-01 14:56:46 -0700

    100-change.c

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main - multiplies two numbers
+ * @argv: arguments of vectors
+ * @argc: arguments of  characters
+ * Return: 0
+ */
+int main(int argc, char *argv[])
+{
+	if (argc != 3)
+	{
+		printf("ERROR\n");
+		return (1);
+	}
+	int num1 = atoi(argv[1]);
+	int num2 = atoi(argv[2]);
+
+	printf("%:
+}
---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <string.h>
+#include <ctype.h>
+#include <stdlib.h>
+
+/**
+ * main - entry point of the programme
+ * @argc: argument count
+ * @argv: argumentt arrays
+ *
+ * Return: o on success
+ */
+int main(int argc, char *argv[])
+{
+	int num, j result = 0;
+	int coins
+}
