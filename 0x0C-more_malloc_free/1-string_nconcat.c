593834bc8ad1639e9acb742648706ca13d847a4e
Kirwa01 <tunenmordecai98@gmail.com>
2023-11-02 07:14:38 -0700

    1-string_nconcat.c

---
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -0,0 +1,49 @@
+#include "main.h"
+
+/**
+ * string_nconcat - concatenates two strings
+ * @s1: string 1
+ * @s2: string 2
+ * @n: character memory
+ *
+ * Return: return concatenated string
+ */
+char *string_nconcat(char *s1, char *s2, unsigned int n)
+{
+	char *result;
+	unsigned int s1len = 0;
+	unsigned int s2len = 0;
+	unsigned int i = 0;
+
+	if (s1 == NULL)
+		return ("");
+	if (s2 == NULL)
+		return ("");
+
+	for (i = 0; s1[i] != '\0'; i++)
+		s1len++;
+	for (i = 0; s2[i] != '\0'; i++)
+		s2len++;
+	result = malloc(sizeof(char) * (s1len + n) + 1);
+
+	if (result == NULL)
+		return (NULL);
+	if (n >= s2len)
+	{
+		for (i = 0; s1[i] != '\0'; i++)
+			result[i] = s1[i];
+		for (i = 0; s2[i] != '\0'; i++)
+			result[s1len + i] = s2[i];
+		result[s1len + i] = '\0';
+	}
+	else
+	{
+		for (i = 0; s1[i] != '\0'; i++)
+			result[i] = s1[i];
+		for (i = 0; i < n; i++)
+			result[s1len + i] = s2[i];
+		result[s1len + i] = '\0';
+	}
+	return (result);
+
+}
